38597c02cf646c897d605eabbd5cbde459340b95
yamagishi-toko <yamagishi-toko@KMS2013-78.kms.local>
2018-05-25 13:04:11 +0900

    課題2の提出です

---
+++ src/bean/NewEmployeeBean.java
@@ -0,0 +1,20 @@
+package bean;
+
+public class NewEmployeeBean {
+    private String name;
+    private int employeeNumber;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+}
